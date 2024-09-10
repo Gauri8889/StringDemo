e6faed8434db865071bb07f156e4fe1066863608
gauri <dahakegauri@4gmail.com>
2024-09-10 11:53:31 +0530

    DemoString.java

---
+++ src/DemoString.java
@@ -0,0 +1,14 @@
+public class DemoString {
+    public static void main(String[] args){
+        String name = "vaibhav";
+        // for to print indexes
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(i);
+            System.out.println(" ");
+        }
+        System.out.println();
+        for (int i = 0; i < name.length(); i++) {
+
+        }
+    }
+}
